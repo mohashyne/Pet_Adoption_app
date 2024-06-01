d6c6cde03d4d35fffcb6f42a6ad47b95491c84fd
MSA <crystalbluetech@gmail.com>
2024-06-01 19:40:58 +0100

    pure react structure

---
+++ src/App.js
@@ -0,0 +1,37 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+    ]);
+}
+ 
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h1", {}, "Adopt A Pet!"),
+        React.createElement(Pet, {
+            animal: "Cat",
+            name: "Luna",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet, 
+            {
+                animal: "Bird",
+                name: "Pepper",
+                breed: "Cockatiel"
+            }
+        ),
+        React.createElement(Pet, 
+            {   animal: "Horse",
+                name: "Doink",
+                breed: "Mixed"
+            }),
+    );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
