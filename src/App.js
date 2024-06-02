0843b3d23f2bea06b035331096717e14ab1f9e37
MSA <crystalbluetech@gmail.com>
2024-06-02 17:24:01 +0100

    npm and eslint setup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
+  return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h3", {}, props.breed),
-    ]);
-}
- 
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "Adopt A Pet!"),
-        React.createElement(Pet, {
-            animal: "Cat",
-            name: "Luna",
-            breed: "Havanese"
-        }),
-        React.createElement(Pet, 
-            {
-                animal: "Bird",
-                name: "Pepper",
-                breed: "Cockatiel"
-            }
-        ),
-        React.createElement(Pet, 
-            {   animal: "Horse",
-                name: "Doink",
-                breed: "Mixed"
-            }),
-    );
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt A Pet!"),
+    React.createElement(Pet, {}),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Luna",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Bird",
+      name: "Pepper",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { animal: "Horse", name: "Doink", breed: "Mixed" })
+  );
 };
 
 const container = document.getElementById("root");
