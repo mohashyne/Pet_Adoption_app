81d228e906d17b7f02581b54d11dfba1de7c792c
MSA <crystalbluetech@gmail.com>
2024-06-08 17:39:06 +0100

    hooks(useEffect) fetch api

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
-const ANIMALS = ["bird", "cats", "rabbit", "reptile"];
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const BREEDS = ["poodele", "kitty"];
+  const [pets, setPets] = useState([]);
+  const breeds = [];
+
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -35,22 +51,31 @@ const SearchParams = () => {
           </select>
         </label>
         <label htmlFor="breed">
-          breed
+          Breed
           <select
             id="breed"
-            disabled={BREEDS.length === 0}
+            disabled={breeds.length === 0}
             value={breed}
             onChange={(e) => {
               setBreed(e.target.value);
             }}
           >
-            {BREEDS.map((breed) => (
+            <option />
+            {breeds.map((breed) => (
               <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>
         <button>Submit</button>
       </form>
+        {pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            key={pet.id}
+          />
+        ))}
     </div>
   );
 };
